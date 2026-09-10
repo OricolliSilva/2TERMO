87240a33060a7ac5c8a47c8a9e48044eb4f95f19
OricolliSilva <oricollidasilvamatheus@gmail.com>
2026-09-10 13:09:57 -0300

    Atualização que era pra ter funcionado

---
+++ PBE/Aula4/Aula4.js
@@ -0,0 +1,13 @@
+// for(let i = 0; i <= 100000; i++) {
+//     const contador = i / 100000;
+//     console.log(contador);
+// }
+
+//---------------------------------------------------------------------------
+
+// usando o operador -= para substituir
+
+for(let i = 10; i >= 0; i -= 1) {
+    console.log(`Contagem regressiva: ${i}`);
+}
+console.log("DECOLAR!");
---
+++ PBE/Aula8/trycat.js
@@ -0,0 +1,20 @@
+function verificarPeso(leitura) {
+
+    const peso = Number(leitura);
+ 
+    if (isNaN(peso)) {
+        throw new Error("Entrada inválida! Digite apenas números.");
+    }
+
+    if (peso < 100) {
+        throw new Error("Peso fora do padrão (100g - 500g)! Peça descartada.");
+    }
+ 
+    if (peso > 500) {
+        throw new Error("Peso fora do padrão (100g - 500g)! Peça descartada.");
+    }
+ 
+    return `Peça aprovada com ${peso}g.`;
+}
+ 
+module.exports = { verificarPeso };
