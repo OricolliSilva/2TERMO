fb018df8ef70caa918871d814d7512a0d1bf2691
Matheus Oricolli <oricollidasilvamatheus@gmail.com>
2026-09-10 08:08:04 -0300

    Commits não feitos

--- PBE/Aula5/arrey.js
+++
@@ -1,3 +0,0 @@
-const alunos = ["ana", "Bruno", "Carlos", "Diego"];
-console.log(`O primeiro aluno da lista é: ${alunos[0]}`);
-console.log(`Quantidade de alunos: ${alunos.length}`); // length mostra o tamanho
