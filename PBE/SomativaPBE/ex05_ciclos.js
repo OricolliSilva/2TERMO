9e6759baae58c4f427d85f568ff9f377c1c5ecc3
OricolliSilva <oricollidasilvamatheus@gmail.com>
2026-09-18 16:19:35 -0300

    Atualizar

--- PBE/SomativaPBE/ex05_ciclos.js
+++
@@ -1,8 +0,0 @@
-const entrada = require('readline-sync');
-
-const pecasCiclo = entrada.questionInt('Digite a quantidade de pecas produzidas por ciclo: ');
-
-for (let i = 1; i <= 10; i++) {
-    const producaoAcumulada = pecasCiclo * i;
-    console.log(`Ciclo ${i}: ${producaoAcumulada} peças acumuladas`);
-}
